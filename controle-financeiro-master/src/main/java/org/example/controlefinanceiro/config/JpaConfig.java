d9aa518a65dbdc5b1f34db661110df640440db05
Gabriela Sena da Silva <87918289+Gabriela-Sena@users.noreply.github.com>
2025-02-21 20:02:22 -0300

    (feat) ✨ como rodar o front

--- controle-financeiro-master/src/main/java/org/example/controlefinanceiro/config/JpaConfig.java
+++ controle-financeiro-master/src/main/java/org/example/controlefinanceiro/config/JpaConfig.java
@@ -7,3 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "org.example.controlefinanceiro.repository")
 public class JpaConfig {
 }
+
+/*rodar front do projeto
+1. abrir o projeto no git
+2. abrir terminal no vs code
+3. rodar 'npm-install' na pasta do projeto para instalar dependencias (só roda na 1 vez)
+4. rodar 'ng- serve' na pasta do projeto */
